ed45ea7fc59ed33af431f43520b7c411b47ca844
Grace Cheng <chengracecwy@gmail.com>
2021-01-16 19:57:04 +0800

    Refactor and add comment

--- src/Test.cpp
+++ src/Test.cpp
@@ -4,57 +4,56 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 int main()
 {
-    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-    default_random_engine generator(seed);
-    uniform_int_distribution<int> randOneToTen(0, 9);
-    uniform_int_distribution<int> randOneToSix(0, 5);
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<int> randOneToTen(0, 9);
+    std::uniform_int_distribution<int> randOneToSix(0, 5);
     unsigned int hp = 15;
-    string allWays[10] = { "Axe", "Arrow", "Helmet", "Shield", "Steal", "Axe (Gold)", "Arrow (Gold)", "Helmet (Gold)", "Shield (Gold)", "Steal (Gold)" };
-    vector<vector<string>> allDices;
-    vector<string> rollOut = {};
-    vector<string> chosen = {};
+    std::string allWays[10] = { "Axe", "Arrow", "Helmet", "Shield", "Steal", "Axe (Gold)", "Arrow (Gold)", "Helmet (Gold)", "Shield (Gold)", "Steal (Gold)" };
+    std::vector<std::vector<std::string>> allDices;
+    std::vector<std::string> rollOut = {};
+    std::vector<std::string> chosen = {};
 
-    // for (size_t i = 0; i < 6; ++i)
-    // {
-    //     cout << "Index: " << randI(generator) << endl;
-    //     cout << "Result: " << dices[randI(generator)] << endl;
-    // }
-
+    // init the player dices
     for (size_t j = 0; j < 6; ++j) {
-        vector<string> temp;
+        std::vector<std::string> temp;
         for (size_t k = 0; k < 6; ++k) {
             temp.emplace_back(allWays[randOneToTen(generator)]);
         }
         allDices.emplace_back(temp);
     }
 
-    for (size_t i = 0; i < 6; ++i) {
-        rollOut.emplace_back(allDices[i][randOneToSix(generator)]);
-    }
-
-    for (vector<string>::const_iterator it = rollOut.begin(); it != rollOut.end(); ++it) {
-        cout << "Dice " << it - rollOut.begin() + 1 << ": " << *it << endl;
-    }
-
-    unsigned int x = 10, round = 0;
+    // asking player to pick out dices (each have 3 round)
+    unsigned int x = -1, round = 0;
     while (x != 0 && round < 3) {
-        cout << "You choose dice? (Enter 0 to end this round): ";
-        cin >> x;
+        // push what dice faces it rolled out to rollOut array
+        for (size_t i = 0; i < 6; ++i) {
+            rollOut.emplace_back(allDices[i][randOneToSix(generator)]);
+        }
+
+        // display the dice faces
+        for (std::vector<std::string>::const_iterator it = rollOut.begin(); it != rollOut.end(); ++it) {
+            std::cout << "Dice " << it - rollOut.begin() + 1 << ": " << *it << std::endl;
+        }
+
+        std::cout << "Enter 0 to end this round" << std::endl;
+        std::cout << "You choose dice? (Enter 0 to end this round): ";
+        std::cin >> x;
         chosen.emplace_back(rollOut.at(x - 1));
+
+        // display choosen disces
+        std::cout << "You choose: ";
+        for (std::vector<std::string>::const_iterator it = chosen.begin(); it != chosen.end(); ++it) {
+            if (it - chosen.begin() == 2) {
+                std::cout << *it;
+            } else {
+                std::cout << *it << ", ";
+            }
+        }
+        std::cout << std::endl;
+        
         ++round;
     }
-
-    cout << "You choose: ";
-    for (vector<string>::const_iterator it = chosen.begin(); it != chosen.end(); ++it) {
-        if (it - chosen.begin() == 2) {
-            cout << *it;
-        } else {
-            cout << *it << ", ";
-        }
-    }
-    cout << endl;
 }
